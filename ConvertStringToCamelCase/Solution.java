2f93fbf750b25db6e80c1d06f51bea0545e79f81
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2023-07-28 22:02:28 -0400

    fix - ConvertStringToCamelCase

--- ConvertStringToCamelCase/Solution.java
+++ ConvertStringToCamelCase/Solution.java
@@ -1,7 +1,15 @@
-import java.lang.StringBuilder;
+package ConvertStringToCamelCase;
 class Solution{
 
   static String toCamelCase(String s){
-    return "";
+    String [] words = s.split("[-_]");
+    StringBuilder res = new StringBuilder(words[0]);
+
+    for (int i = 1; i < words.length; i++) {
+      res.append(words[i].substring(0,1).toUpperCase());
+      res.append(words[i].substring(1));
+    }
+
+    return res.toString();
   }
 }
--- ConvertStringToCamelCase/SolutionTest.java
+++ ConvertStringToCamelCase/SolutionTest.java
@@ -1,3 +1,4 @@
+package ConvertStringToCamelCase;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import org.junit.runners.JUnit4;
