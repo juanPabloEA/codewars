370b1dd5f8bb8b87de6e68c1418ec93adcf8b8aa
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2023-07-28 22:23:43 -0400

    clever - solvet with Pattern Decode The Morse Code

--- DecodeTheMorseCode01/MorseCodeDecoder.java
+++ DecodeTheMorseCode01/MorseCodeDecoder.java
@@ -1,16 +1,23 @@
 package DecodeTheMorseCode01;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class MorseCodeDecoder {
+    private static final String WORD_DELIMITER = "   ";
+    private static final String LETTER_DELIMITER = " ";
+    
+    
     public static String decode(String morseCode) {
-        return Arrays.stream(morseCode.trim().split("   "))
+        Pattern pattern = Pattern.compile(WORD_DELIMITER);
+        return pattern.splitAsStream(morseCode.trim())
                 .map(MorseCodeDecoder::decodeWord)
                 .collect(Collectors.joining(" "));
     }
 
     private static String decodeWord(String word) {
-        return Arrays.stream(word.split(" ")).map(MorseCode::get).collect(Collectors.joining());
+        Pattern pattern = Pattern.compile(LETTER_DELIMITER);
+        return pattern.splitAsStream(word).map(MorseCode::get).collect(Collectors.joining());
     }
 }
