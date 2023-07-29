4bbb32a6d75e099feef11398aafcce792f13f1e6
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2023-07-28 22:14:00 -0400

    clever - The Morse Code Decoder

--- DecodeTheMorseCode01/MorseCodeDecoder.java
+++ DecodeTheMorseCode01/MorseCodeDecoder.java
@@ -1,7 +1,16 @@
 package DecodeTheMorseCode01;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class MorseCodeDecoder {
     public static String decode(String morseCode) {
-        // your brilliant code here, remember that you can access the preloaded Morse code table through MorseCode.get(code)
+        return Arrays.stream(morseCode.trim().split("   "))
+                .map(MorseCodeDecoder::decodeWord)
+                .collect(Collectors.joining(" "));
+    }
+
+    private static String decodeWord(String word) {
+        return Arrays.stream(word.split(" ")).map(MorseCode::get).collect(Collectors.joining());
     }
 }
