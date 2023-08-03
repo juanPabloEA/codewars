c05d57e49d6f09ff7ea5ad2997a6dc2d9f3c5b94
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2023-08-02 21:53:11 -0400

    refactor - Strip Comments

--- StripComments/StripComments/StripComments.java
+++ StripComments/StripComments/StripComments.java
@@ -1,22 +1,17 @@
 package StripComments;
 import java.util.Arrays;
-import java.util.List;
+import java.util.stream.Collectors;
 
 public class StripComments {
 
 	public static String stripComments(String text, String[] commentSymbols) {
-		return Arrays.stream(text.split("\n"))
-			.map(line -> {
-				final int index = Arrays.stream(commentSymbols)
-					.map(line::indexOf)
-					.filter(i -> i > -1)
-					.min(Integer::compare)
-					.orElse(-1);
-				return index > -1 ? line.substring(0, index) : line;
-			})
-			.map(line -> line.replaceAll("\\s+$", ""))
-			.reduce((a, b) -> a + "\n" + b)
-			.get();
+		String pattern = String.format(
+				"[ ]*([%s].*)?$",
+				Arrays.stream( commentSymbols ).collect( Collectors.joining() )
+		);
+		return Arrays.stream( text.split( "\n" ) )
+				.map( x -> x.replaceAll( pattern, "" ) )
+				.collect( Collectors.joining( "\n" ) );
 	}
 	
 }
