2dfb85ab376c936884a2c429817f400795aafce3
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2023-08-02 22:24:21 -0400

    refactor - Secure Tester

--- NotVerySecure/SecureTester.java
+++ NotVerySecure/SecureTester.java
@@ -4,14 +4,7 @@ public class SecureTester{
         if (s == null || s.isEmpty()) {
             return false;
         }
-        for (char c : s.toCharArray()) {
-            if('0'<=c && c<='9') continue;
-            if('a'<=c && c<='z') continue;
-            if('A'<=c && c<='Z') continue;
-            if(c=='-') continue;
-            return false;
-            
-        }
-        return true;
+        
+        return s.replaceAll("[^a-zA-Z0-9]", "").length() == s.length();
     }
   }
