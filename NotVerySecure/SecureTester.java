9821e61cdfbbb9abd413630811eae358905075b7
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2023-08-02 22:26:17 -0400

    refactor - Secure Tester

--- NotVerySecure/SecureTester.java
+++ NotVerySecure/SecureTester.java
@@ -1,10 +1,6 @@
 public class SecureTester{
   
     public static boolean alphanumeric(String s){
-        if (s == null || s.isEmpty()) {
-            return false;
-        }
-        
-        return s.replaceAll("[^a-zA-Z0-9]", "").length() == s.length();
+        return s.matches("[^a-zA-Z0-9]+");
     }
   }
