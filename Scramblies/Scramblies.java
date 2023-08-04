8ccca0196a6e43e03dbb3679458c39a473f339b8
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2023-08-03 20:53:54 -0400

    refactor - Scramblies

--- Scramblies/Scramblies.java
+++ Scramblies/Scramblies.java
@@ -1,16 +1,13 @@
 import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Scramblies {
     
     public static boolean scramble(String str1, String str2) {
-        StringBuilder resBuilder = new StringBuilder(str1);
-        for (char letter : str2.toCharArray()) {
-            int index = resBuilder.indexOf(letter + "");
-            if (index == -1) {
-                return false;
-            }
-            resBuilder.replace(index, index + 1, "");
-        }
-        return str1.length() - str2.length() == resBuilder.length();
+        Map<String, Long> str1Map = Stream.of(str1.split("")).collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        Map<String, Long> str2Map = Stream.of(str2.split("")).collect(Collectors.groupingBy(c -> c, Collectors.counting()));
+        return str2Map.entrySet().stream().allMatch(e -> str1Map.get(e.getKey()) != null && e.getValue() <= str1Map.get(e.getKey()));
     }
 }
