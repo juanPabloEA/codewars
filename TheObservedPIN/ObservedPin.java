fbac894f0f79d8683734da38a84fe200bb1c942f
Juan Pablo Espinoza Alvial <jpabloea93@gmail.com>
2023-08-16 14:44:18 -0400

    refactor -  The Observed Pin

--- TheObservedPIN/ObservedPin.java
+++ TheObservedPIN/ObservedPin.java
@@ -2,63 +2,37 @@ package TheObservedPIN;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
 
 public class ObservedPin {
-    private static String[][] keyboard = new String[][] {
-            { "1", "2", "3" },
-            { "4", "5", "6" },
-            { "7", "8", "9" },
-            { "", "0", "" }
-    };
+     private static final Map<Character,String> ADJACENTS = new HashMap<Character,String>() {{
+        put('1', "124");
+        put('2', "2135");
+        put('3', "326");
+        put('4', "4157");
+        put('5', "54268");
+        put('6', "6953");
+        put('7', "748");
+        put('8', "87590");
+        put('9', "986");
+        put('0', "08");
+    }};
+
     public static List<String> getPINs(String observed) {
-        List<String> pChar = Arrays.asList(observed.split(""));
-        List<List<String>> result = new java.util.ArrayList<List<String>>();
-
-        for (String key : pChar) {
-            result.add(getTheButtonAround(key));
-        }
-
-        return generateCombinations(result, 0);
-    } 
-
-    public static List<String> generateCombinations(List<List<String>> matrix, int index) {
-        List<String> combinations = new ArrayList<>();
         
-        if (index == matrix.size()) {
-            combinations.add("");
-            return combinations;
-        }
+        List<String> ans = Arrays.asList("");
         
-        List<String> currentList = matrix.get(index);
-        List<String> restCombinations = generateCombinations(matrix, index + 1);
-
-        for (String item : currentList) {
-            for (String rest : restCombinations) {
-                combinations.add(item + rest);
+        for (char c: observed.toCharArray()) {
+            
+            List<String> tmp = new ArrayList<String>();
+            for (char cc: ADJACENTS.get(c).toCharArray()) {
+                for (String s: ans) tmp.add(s+cc);
             }
+            ans = tmp;
         }
-
-        return combinations;
-    }
-    
-     private static List<String> getTheButtonAround(String key) {
-        for (int row = 0; row < keyboard.length; row++) {
-            for (int col = 0; col < keyboard[row].length; col++) {
-                if (key.equals(keyboard[row][col])) {
-                    return Arrays.asList(
-                            row - 1 >= 0 ? keyboard[row - 1][col] : "",
-                            row + 1 < keyboard.length ? keyboard[row + 1][col] : "",
-                            col - 1 >= 0 ? keyboard[row][col - 1] : "",
-                            col + 1 < keyboard[row].length ? keyboard[row][col + 1] : "",
-                            keyboard[row][col]).stream()
-                            .filter(s -> !s.equals(""))
-                            .collect(Collectors.toList());
-                }
-            }
-        }
-        return new ArrayList<String>();
+        return ans;
     }
 
 } // ObservedPin
